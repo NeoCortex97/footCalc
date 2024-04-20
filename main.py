eb54a25f058f25376807925bcb3ac6a776520d41
schmelzs <stefan.schmelz@solupharm.com>
2024-04-20 19:19:02 +0200

    code cleanup and refactoring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pathlib
-from typing import List
+from typing import List, Tuple, Set
 
 import matplotlib.axes
 import pandas as pd
@@ -24,11 +24,11 @@ class MeasurementRange:
 
 
 class Dataset:
-    def __init__(self):
-        self.filename: pathlib.Path = pathlib.Path()
-        self.sheet_name: str = ''
+    def __init__(self, filename: pathlib.Path, sheet: str):
+        self.filename: pathlib.Path = filename
+        self.sheet_name: str = sheet
         self.measurement_ranges: List[MeasurementRange] = []
-        self.data_frame: pd.DataFrame = None
+        self.data_frame: pd.DataFrame = pd.DataFrame()
         self.max_rows: int = 0
         self.plot: matplotlib.axes.Axes = None
 
@@ -39,15 +39,31 @@ class Dataset:
                 + ', '.join([str(f) for f in self.measurement_ranges]))
 
 
-datasets: List[Dataset] = [] # This contains all files
+def calc_avg(df: pd.DataFrame, destination: str, field: str, count: int):
+    df[destination] = df.apply(
+        lambda row: sum([row[f'{field}{i}'] for i in range(count)]) / count,
+        axis=1)
 
-# Iterate over all files and scan datasets.
-for file in [f for f in path.iterdir() if f.is_file() and f.name.endswith('.xlsx')]:
+
+def calc_axes(df: pd.DataFrame, column: str, padding: int = 10) -> tuple[int, int]:
+    v_max: int = df[column].max()
+    v_min: int = df[column].min()
+    if pd.isna(v_max):
+        v_max = 10
+    if pd.isna(v_min):
+        v_min = 10
+
+    value: int = max(abs(v_min), abs(v_max))
+    padded: int = value + padding
+    print(column, v_min, v_max, 'Absolut:', value, 'Padded:', padded)
+
+    return padded * -1, padded
+
+
+def scan_file(file: pathlib.Path) -> Dataset:
     wb = load_workbook(file, False, False, False, False, False)
-    datasets.append(Dataset())
-    datasets[-1].filename = file
-    datasets[-1].sheet_name = wb.sheetnames[0]
-    ws = wb[datasets[-1].sheet_name]
+    dataset = Dataset(file, wb.sheetnames[0])
+    ws = wb[dataset.sheet_name]
     r = MeasurementRange(0, 0)
     active = False
     for cell in ws['A']:
@@ -56,53 +72,55 @@ for file in [f for f in path.iterdir() if f.is_file() and f.name.endswith('.xlsx
             active = True
         if active and cell.value is None:
             r.end = cell.row
-            datasets[-1].measurement_ranges.append(r)
+            dataset.measurement_ranges.append(r)
             r = MeasurementRange(0, 0)
             active = False
 
-    datasets[-1].max_rows = max([r.end - r.start for r in datasets[-1].measurement_ranges])
+    dataset.max_rows = max([r.end - r.start for r in dataset.measurement_ranges])
+    return dataset
 
 
-def average(data: list) -> float:
-    pass
+def load_ranges(dataset: Dataset):
+    for r in dataset.measurement_ranges:
+        df = pd.read_excel(str(dataset.filename),
+                           sheet_name=dataset.sheet_name,
+                           header=r.start - 1,
+                           index_col=0,
+                           usecols='A:E',
+                           nrows=r.end - r.start)
+        r.df = df
+
+
+def populate_common_df(dataset: Dataset):
+    # Merge dataframes
+    dataset.data_frame.index.name = 'Frame'  # Set name of index column
+    dataset.data_frame['t'] = sorted([f.df for f in dataset.measurement_ranges], key=len)[-1]['ms']
+    for index, r in enumerate(dataset.measurement_ranges):
+        dataset.data_frame[f'x{index}'] = r.df['X (mm)']
+        dataset.data_frame[f'y{index}'] = r.df['Y (mm)']
+        dataset.data_frame[f'f{index}'] = r.df[f'Force (N)']
+
+    # Calculate averages for x, y and force
+    calc_avg(dataset.data_frame, 'avg_x', 'x', len(dataset.measurement_ranges))
+    calc_avg(dataset.data_frame, 'avg_y', 'y', len(dataset.measurement_ranges))
+    calc_avg(dataset.data_frame, 'avg_f', 'f', len(dataset.measurement_ranges))
 
 
 def main():
+    datasets: List[Dataset] = []  # This contains all files
+
+    # Iterate over all files and scan datasets.
+    for file in [f for f in path.iterdir() if f.is_file() and f.name.endswith('.xlsx')]:
+        datasets.append(scan_file(file))
+
     # Load data-frames for every range
     for dataset in datasets:
-        for r in dataset.measurement_ranges:
-            df = pd.read_excel(str(dataset.filename),
-                               sheet_name=dataset.sheet_name,
-                               header=r.start - 1,
-                               index_col=0,
-                               usecols='A:E',
-                               nrows=r.end - r.start)
-            r.df = df
+        load_ranges(dataset)
 
-        # Merge dataframes
-        dataset.data_frame = pd.DataFrame()
-        dataset.data_frame.index.name = 'Frame'  # Set name of index column
-        dataset.data_frame['t'] = sorted([f.df for f in dataset.measurement_ranges], key=len)[-1]['ms']
-        for index, r in enumerate(dataset.measurement_ranges):
-            dataset.data_frame[f'x{index}'] = r.df['X (mm)']
-            dataset.data_frame[f'y{index}'] = r.df['Y (mm)']
-            dataset.data_frame[f'f{index}'] = r.df[f'Force (N)']
-
-        # Calculate averages for x, y and force
-        dataset.data_frame['avg_x'] = dataset.data_frame.apply(
-            lambda row: sum([row[f'x{i}'] for i in range(len(dataset.measurement_ranges))])/len(dataset.measurement_ranges),
-            axis=1)
-        dataset.data_frame['avg_y'] = dataset.data_frame.apply(
-            lambda row: sum([row[f'y{i}'] for i in range(len(dataset.measurement_ranges))]) / len(
-                dataset.measurement_ranges),
-            axis=1)
-        dataset.data_frame['avg_f'] = dataset.data_frame.apply(
-            lambda row: sum([row[f'f{i}'] for i in range(len(dataset.measurement_ranges))]) / len(
-                dataset.measurement_ranges),
-            axis=1)
+        populate_common_df(dataset)
 
         # Kill all rows that contain empty values.
-        # dataset.data_frame.dropna(axis=0, inplace=True)
+        dataset.data_frame.dropna(axis=0, inplace=True)
 
         print(dataset)
         print(dataset.data_frame)
@@ -111,6 +129,16 @@ def main():
         # Add Axis lines
         dataset.plot.axhline(c='grey', lw=1)
         dataset.plot.axvline(c='grey', lw=1)
+
+        # Set Axis range
+        ## Get min and max value of x dataset
+        x_min, x_max = calc_axes(dataset.data_frame, 'avg_x')
+
+        ## Get min and max value of y dataset
+        y_min, y_max = calc_axes(dataset.data_frame, 'avg_y')
+
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
 
         # Draw Ellipse over plot
         ellipse = Ellipse((0, 0), 5, 8, facecolor='none', edgecolor='red')
