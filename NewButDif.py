3bfe28d3731c02f7cf73285094f11790aeeda8f1
pc <@>
2024-04-23 00:35:39 +0200

    tedst

--- NewButDif.py
+++ NewButDif.py
@@ -1,25 +1,57 @@
 from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter
+#from openpyxl.utils import get_column_letter
+import pandas as pd
 
-wb = load_workbook('data/Simon_Schmlez_-_Basketball_SLJ_SS_BF_-_21-11-2023_-_Center_of_Force_line.xlsx')
+def read_clean_val(messungen, start_list, end_list,Data):
+   i = 0
+   data = []
+   while i < messungen:
+      start = start_list[i]
+      end = end_list[i]
+      #print(Data_x[start:end])
+      data.append(Data[start:end])
+      i += 1
+   #print(Data_x)
+   return (data)
+
+def get_range (start = [], end = []):
+   active = False
+   for cell in ws['A']:
+      if cell.value == 'Frame':
+         start.append(cell.row)
+         active = True
+      #print(cell.value)
+      if active and cell.value is None:
+         end.append(cell.row-1)
+         active = False
+   return (start, end)
+
+def get_data ():
+   for cell in ws['C']:
+      Data_x.append(cell.value)
+   for cell in ws['D']:
+      Data_y.append(cell.value)
+   return (Data_x, Data_y)
+
+
+path = "data/Simon_Schmlez_-_Basketball_SLJ_SS_BF_-_21-11-2023_-_Center_of_Force_line.xlsx"
+wb = load_workbook(path)
 ws = wb.active
-# print(ws)
+Data_x = []
+Data_y = []
+Data_x, Data_y = get_data()
+start_list, end_list = get_range()
 
-start_list = []
-end_list = []
-active = False
-
-for cell in ws['C']:
-   print(cell.value)
-   if cell.value == "X (mm)":
-      start_list.append(cell.row)
-      active = True
-   if active and cell.value is None:
-      end_list.append(cell.row-1)
-      active = False
-
-messungen = str(len(start_list))
-print("\nAnzahl der Messungen: " + messungen+"\n")
+end_list.append(len(ws['c']))
+messungen = (len(start_list))
+print("Anzahl der Messungen: ", messungen)
 print(start_list)
 print(end_list)
 
+data_x = read_clean_val(messungen, start_list, end_list, Data_x)
+data_y = read_clean_val(messungen, start_list, end_list, Data_y)
+#print(data_x)
+#print(data_x[2])
+#print(Data_y)
+print(data_y[0])
+print(data_x[0])
