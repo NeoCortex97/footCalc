18eadcc69e4fea5734479115bc44804b56c1fdeb
pc <@>
2024-06-03 12:04:11 +0200

    tedst

--- NewButDif.py
+++ NewButDif.py
@@ -1,22 +1,28 @@
+import sys
+
 import pandas as pd
+import openpyxl
 from openpyxl import load_workbook
 import numpy as np
 import tkinter as tk
 from tkinter import filedialog
+import os
+import asposecells
+
 
 #from openpyxl.utils import get_column_letter
-
+path = ""
 def get_file_path():
-    selected_file_path = ""
+    global path
     # Dateidialog öffnen, um einen Dateipfad auszuwählen
-    selected_file_path = filedialog.askopenfilename()
+    path = filedialog.askopenfilename()
 
     # Den ausgewählten Dateipfad anzeigen
-    if selected_file_path:
-        path_label.config(text="Dateipfad: " + selected_file_path)
+    if path:
+        path_label.config(text="Dateipfad: " + path)
     else:
         path_label.config(text="Keine Datei ausgewählt")
-    return selected_file_path
+
 
 def Hauptfenster():
     root = tk.Tk()
@@ -24,7 +30,9 @@ def Hauptfenster():
     root.geometry("900x400")
     # Button zum Öffnen des Dateidialogs
     browse_button = tk.Button(root, text="Datei auswählen", command=get_file_path)
+    browse_button2 = tk.Button(root, text="Bestätigen", command=root.destroy)
     browse_button.pack(pady=10)
+    browse_button2.pack(pady=10)
 
     # Label zur Anzeige des ausgewählten Dateipfads
     global path_label
@@ -80,9 +88,9 @@ def get_df(data):
         h+=1
 
     for k in range(len(data)):
-        data[k]=np.array(data[k][:minLen])
+        data[k] = np.array(data[k][:minLen])
 
-    df=pd.DataFrame(
+    df = pd.DataFrame(
         {
             'LinFus1': data[0],
             'LinFus2': data[1],
@@ -94,29 +102,44 @@ def get_df(data):
     )
     return df
 
+def check_path():
+    global path
+    if ".xls"  in path and ".xlsx" not in path:
+        print("Keine Valide Datei")
+        return "NotX"
+
+def auswerutng() :
+    #if check_path() != "NotX":
+      #  wb = load_workbook(path)
+      #  ws = wb.active
+        Data_x, Data_y = get_data()
+        start_list, end_list = get_range()
+        end_list.append(len(ws['c']))
+        messungen = (len(start_list))
+
+        print("Anzahl der Messungen: ", messungen)
+        print(start_list)
+        print(end_list)
+
+        data_x = read_clean_val(messungen, start_list, end_list, Data_x)
+        data_y = read_clean_val(messungen, start_list, end_list, Data_y)
+        #print(data_x)
+        #print(data_x[2])
+        #print(Data_y)
+        print(data_y[2])
+        print(data_x[2])
+
+        df_x = get_df(data_x)
+        df_y = get_df(data_y)
+
+        print(df_x)
+        print(df_y)
+
+
+
+
 Hauptfenster()
-path = get_file_path()
-wb = load_workbook(path)
-ws = wb.active
-Data_x, Data_y = get_data()
-start_list, end_list = get_range()
-end_list.append(len(ws['c']))
-messungen = (len(start_list))
-
-print("Anzahl der Messungen: ", messungen)
-print(start_list)
-print(end_list)
-
-data_x = read_clean_val(messungen, start_list, end_list, Data_x)
-data_y = read_clean_val(messungen, start_list, end_list, Data_y)
-#print(data_x)
-#print(data_x[2])
-#print(Data_y)
-print(data_y[2])
-print(data_x[2])
-
-df_x = get_df(data_x)
-df_y = get_df(data_y)
-
-print(df_x)
-print(df_y)
+if check_path() != "NotX":
+    wb = load_workbook(path)
+    ws = wb.active
+    auswerutng()
